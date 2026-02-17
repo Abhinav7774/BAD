f204b709aea9c9f57a2e91b572d044996f6a70c9
DarkFlame774 <gargabhi1505@gmail.com>
2026-02-18 00:05:56 +0530

    Repair Cache

--- BAD/src/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "Test.h"
-#include <cstdio>
-
-namespace BAD {
-
-	void Print() {
-		printf("Welcome to BAD Engine\n");
-	}
-}
--- Sanbox/src/Application.cpp
+++
@@ -1,7 +0,0 @@
-namespace BAD {
-	_declspec(dllimport) void Print();
-}
-
-void main() {
-	BAD::Print();
-}
